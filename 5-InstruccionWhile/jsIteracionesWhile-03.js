249767ff6d5876ca280d4928e40728f652e1aae4
CarnelosJoaquin <98614427+CarnelosJoaquin@users.noreply.github.com>
2022-02-18 13:29:37 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,15 @@
 /*
+While 3; Carnelos Duarte Joaquin Alejo */
+/*
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
 	var claveIngresada;
 	claveIngresada = prompt("ingrese el número clave.");
-	
-}//FIN DE LA FUNCIÓN
+
+	while (claveIngresada != "utn750"){
+		alert("La clave es incorrecta.");
+		claveIngresada = prompt("Reingrese la clave");
+	}
+	alert("La clave "+ claveIngresada+" es correcta.")
+}
--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,15 @@
 /*
+While 4; Carnelos Duarte Joaquin Alejo */
+/*
 al presionar el botón 
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
-}//FIN DE LA FUNCIÓN
+	let numeroIngresado = parseInt(prompt("ingrese un número entre 0 y 9."));
+
+	while(!(numeroIngresado>=0 && numeroIngresado<=9)){
+		alert("N° incorrecto.");
+		numeroIngresado = parseInt(prompt("ingrese un número entre 0 y 9."));
+	}
+	document.getElementById('txtIdNumero').value= numeroIngresado;
+}
