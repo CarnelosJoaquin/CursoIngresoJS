7483d69197e1b0a3db3e2fadc6105a6f950919af
CarnelosJoaquin <98614427+CarnelosJoaquin@users.noreply.github.com>
2022-02-20 17:18:58 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,12 +1,12 @@
 /*
+While 5; Carnelos Duarte Joaquin Alejo *//*
 Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
 {
-	var sexoIngresado;
-	sexoIngresado = prompt("ingrese f ó m .");
-
-
-
-	txtIdSexo.value=sexoIngresado;
-}//FIN DE LA FUNCIÓN
+	let sexoIngresado = prompt("ingrese sexo: f ó m .");
+	while(!(sexoIngresado=="f"||sexoIngresado=="F"||sexoIngresado=="m"||sexoIngresado=="M")){
+		sexoIngresado = prompt("Error, ingrese sexo: f ó m .");
+	}
+	document.getElementById('txtIdSexo').value= sexoIngresado;
+}
--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,22 @@
+/*
+While 6; Carnelos Duarte Joaquin Alejo */
+/*
+Enunciado:
+Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.*/
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let contador=0;
+	let acumulador=0;
+	let numero=0;
+	let promedio=0;
+	let i=0;
 
-	contador=0;
-	acumulador=0;
-	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+	while(i<5){
+		numero = parseInt(prompt("Ingrese un número"));
+		acumulador = acumulador + numero;
+		contador = contador+1;
+		i=i+1;
+	}
+	document.getElementById("txtIdSuma") .value=acumulador;
+	document.getElementById("txtIdPromedio").value=acumulador/5;
+}
