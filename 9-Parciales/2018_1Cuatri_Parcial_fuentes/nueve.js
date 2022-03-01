bb0617e4f87e4397a9b7e24454391fe4361a7c54
CarnelosJoaquin <98614427+CarnelosJoaquin@users.noreply.github.com>
2022-03-01 16:17:00 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,76 @@
+/*Parcial 2018 Ej 9, Carnelos Duarte Joaquín Alejo */
+/*Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100 (validar),
+la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera
+e informar al terminar el ingreso por document.write:
+a) La cantidad de temperaturas pares.
+b) La marca del producto más pesado
+c) La cantidad de productos que se conservan a menos de 0 grados.
+d) El promedio del peso de todos los productos.
+f) El peso máximo y el mínimo.
+
+Testeo con los siguientes valores
+(m=pepito;p = 50; t =-300(mal) ,-30(bien) )
+(m=teem;p = 10; t =0 )
+(m=llut;p = 150(mal), 15(bien); t =-13 )
+(m=fpy;p = 45; t =-12 )*/
+
 function mostrar()
 {
+	let marca;
+	let marcaPesada;
+	let marcaLiviana;
+	let peso;
+	let pesoPesado=0;
+	let pesoLiviano=0;
+	let pesoAcumulado=0;
+	let promedioPeso;
+	let temperatura;
+	let temperaturasPares=0;
+	let temperaturaNegativa=0;
+	let respuesta;
+	let bandera=0
+	let cantidad=0
+
+
+	do{
+		marca =(prompt("Ingrese una marca: pepito/teem/llut/fpy"))
+		while(!(marca=="pepito"||marca=="teem"||marca=="llut"||marca=="fpy")){
+			marca =prompt("Por favor, ingrese una marca: pepito/teem/llut/fpy");
+		}
+		peso=parseInt(prompt("Ingrese un peso entre 1 y 100 kg inclusives."));
+		while(isNaN(peso)||(peso>101||peso<1)){
+			peso=(prompt("Por favor, ingrese un peso entre 1 y 100 kg inclusives."));
+		}
+		temperatura=parseInt(prompt("Ingrese una temperatura entre -30°C y 30°C inclusives."));
+		while(isNaN(temperatura)||(temperatura>31||temperatura<-31)){
+			temperatura=(prompt("Por favor, ingrese una temperatura entre -30°C y 30°C inclusives."));
+		}
+		if (temperatura%2==0) {
+			temperaturasPares++;
+		}
+		if(peso>pesoPesado||bandera==0){
+			pesoPesado=peso;
+			marcaPesada=marca;
+		}
+		if(peso<pesoLiviano||bandera==0){
+			pesoLiviano=peso;
+			bandera=1;
+		}
+		if (temperatura<0) {
+			temperaturaNegativa++;
+		}
+		cantidad++;
+		pesoAcumulado=pesoAcumulado+peso;
+		promedioPeso= Math.round(pesoAcumulado/cantidad);
+		respuesta=prompt("Desea ingresar los datos de otro producto: si/no");
+	}while(respuesta == "si"||respuesta=="Si"||respuesta=="Sí"||respuesta=="sí");
+	document.write("La cantidad de temperaturas pares es "+temperaturasPares+".<br>");
+	document.write("La marca del producto mas pesado es "+marcaPesada+".<br>");
+	document.write("La cantidad de productos con temperatura menor a 0°C es "+temperaturaNegativa+".<br>");
+	document.write("El promedio del peso de todos los productos "+promedioPeso+"kg.<br>");
+	document.write("El peso máximo fue "+pesoPesado+"kg y el peso mínimo fue " +pesoLiviano+"kg.<br>");
 
 }
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,47 @@
+/*Parcial 2018 Ej 7, Carnelos Duarte Joaquín Alejo*/
+/*Bienvenidos.
+Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10),
+el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert:
+a) El promedio de las notas totales.
+b) La nota más baja y el sexo de esa persona.
+c) La cantidad de varones que su nota haya sido mayor o igual a 6*/
 function mostrar()
 {
+	let i;
+	let sexo;
+	let nota;
+	let notaMenor=0;
+	let sexoMenor;
+	let acumulador=0
+	let notaAcumulada=0;
+	let promedio=0;
+	let varonesAprobados=0;
+	let bandera=0
 
+	for(i=0;i<=4;i++){
+		sexo=prompt("Ingrese sexo: f/m");{
+			nota=parseInt(prompt("Ingre nota del alumno: 0-10"));
+			while(isNaN(nota)||nota<0||nota>10){
+				nota=parseInt(prompt("Por favor, ingrese nota del alumno: 0-10"));
+			}
+		}
+		while(sexo!="f"&&sexo!="m"){
+			sexo=prompt("Por favor,Ingrese sexo: f/m");
+		}
+		if(nota>5){
+			if(sexo=="m"){
+				varonesAprobados++;
+			}
+		}
+		if (!(nota>=notaMenor)||bandera==0) {
+			notaMenor=nota;
+			sexoMenor=sexo;
+			bandera=1;
+		}
+		notaAcumulada=notaAcumulada+nota;
+	}
+	promedio=Math.round(notaAcumulada/5);
+	alert("El promedio de las notas totales es " + promedio+" .");
+	alert("La nota mas baja es " + notaMenor+" y el sexo de esa persona es "+sexoMenor+".");
+	alert("La cantidad de varones aprobados fue de "+varonesAprobados+".");
 }
