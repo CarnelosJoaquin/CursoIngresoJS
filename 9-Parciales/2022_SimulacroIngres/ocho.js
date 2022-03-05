1753d448ed85c54f03b045f86c0dc8e4556df3c1
CarnelosJoaquin <98614427+CarnelosJoaquin@users.noreply.github.com>
2022-03-05 11:42:38 -0300

    Add files via upload

--- 9-Parciales/2022_SimulacroIngres/ocho.js
+++ 9-Parciales/2022_SimulacroIngres/ocho.js
@@ -1,5 +1,205 @@
+/*Simulacro 8, Carnelos Duarte Joaquín Alejo*/
+/*Mostrar información
+Enunciado:
 
+Para un censo municipal debemos cargar los datos de TODAS las mascotas de nuestra veterinaria
+necesitan saber si es un gato o un perro u "otra cosa"(solo hay esos tres tipos)
+el tipo de pelaje (corto, largo, sin pelo),
+la edad de la mascota (edad validada);
+el nombre (una palabra)
+la raza (una palabra)
+el peso (peso validado)
+el estadoClinico (enfermo, internado o adopción)
+la temperaruta corporal y nos piden informar solo si existe
+a)El perro más pesado
+b)El porcentaje de enfermos sobre el total de mascotas
+c)El nombre de la última mascota de tipo "otra cosa"
+d)El animal sin pelo con menor temperatura corporal
+e)Qué tipo de mascota(gato o un perro u "otra cosa") que tiene el mayor promedio de temperatura corporal
+f)Sumando gatos y perros que porcentaje del total de mascotas son?
+g)Qué estado clínico tiene la menor cantidad de mascotas
+H)Cuál es el promedio de kilos de peso, tomando todas las mascotas
+i)El nombre y raza del gato sin pelos más liviano*/
 function mostrar()
 {
-	alert("uno");
+	let mascota;
+	let pelaje;
+	let nombre;
+	let raza;
+	let peso;
+	let estadoClinico;
+	let temperaturaCorporal;
+	let respuesta;
+	let pesoPerroPesado=0;
+	let nombrePerroPesado;
+	let cantPerros=0;
+	let cantGatos=0;
+	let cantOtros=0;
+	let cantInternados=0;
+	let cantAdopcion=0;
+	let cantEnfermos=0;
+	let promedioEnfermos;
+	let ultimaTipoOTra;
+	let sinPeloFrio=0;
+	let animalFrioSinPelo;
+	let bandera=0;
+	let sinPelo=0;
+	let acumTempPerro=0;
+	let acumTempGato=0;
+	let acumTempOtro=0;
+	let promedioTempPerro;
+	let promedioTempGato;
+	let promedioTempOtro;
+	let mayorPromedio;
+	let promedioCatDog;
+	let menorEstado;
+	let acumuladorKilos=0;
+	let promedioKilos;
+	let sinPeloLiviano;
+	let bandera2=0;
+	let nombreSinPeloLiviano;
+	let razaSinPeloLiviano;
+	let cantGatoSinPelo=0
+
+	do{
+		mascota=prompt("Ingrese mascota: gato/perro/otra cosa");
+		while(!(mascota=="gato"||mascota=="perro"||mascota=="otra cosa")) {
+			mascota=prompt("Por favor,Ingrese mascota: gato/perro/otra cosa");
+		}
+		pelaje=prompt("Ingrese pelaje: corto/largo/sin pelo");
+		while(!(pelaje=="corto"||pelaje=="largo"||pelaje=="sin pelo")) {
+			pelaje=prompt("Por favor,Ingrese pelaje: corto/largo/sin pelo");
+		}
+		if(mascota!="otra cosa"){
+			edad=parseInt(prompt("Ingrese años de edad(entre 1 y 20)."));
+    		while(isNaN(edad)||edad>20||edad<1){
+      			edad=(prompt("Por favor,Ingrese años de edad(entre 1 y 20)"));
+    		}
+    		peso=parseFloat(prompt("Ingrese peso en KG(1-40"));{
+    			while(isNaN(peso)||peso<1||peso>40){
+    				peso=parseFloat(prompt("Por favor, Ingrese peso en KG(1-40"));
+    			}
+    		}
+		}
+		else{
+			edad=parseInt(prompt("Ingrese años de edad(entre 1 y 80)."));
+    		while(isNaN(edad)||edad>80||edad<1){
+      			edad=(prompt("Por favor,Ingrese años de edad(entre 1 y 80)"));
+    		}
+    		peso=parseFloat(prompt("Ingrese peso en KG(1-80"));
+    		while(isNaN(peso)||peso<1||peso>80){
+    			peso=parseFloat(prompt("Por favor, Ingrese peso en KG(1-80"));
+    		}
+		}
+    	raza=prompt("Ingrese raza:");
+		while(!(isNaN(raza))){
+			raza=prompt("Por favor,Ingrese raza:");
+		}
+		estadoClinico=prompt("Ingrese estado clínico: enfermo/internado/adopción");
+		while(!(estadoClinico=="enfermo"||estadoClinico=="internado"||estadoClinico=="adopción")){
+			estadoClinico=prompt("Por favor,Ingrese estado clínico: enfermo/internado/adopción");
+		}
+		temperaturaCorporal=parseFloat(prompt("Ingrese temperaturaCorporal en °C"));
+		while(isNaN(temperaturaCorporal)){
+			temperaturaCorporal=parseFloat(prompt("Por favor, ingrese una temperatura real en °C"));
+		}
+		switch(mascota){
+			case"perro":
+			cantPerros++;
+			if(peso>pesoPerroPesado){
+				pesoPerroPesado=peso;
+				nombrePerroPesado=nombre;
+			}
+			acumTempPerro=acumTempPerro+temperaturaCorporal;
+			promedioTempPerro=acumTempPerro/cantPerros;
+
+			break;
+			case"gato":
+			cantGatos++;
+			acumTempGato=acumTempGato+temperaturaCorporal;
+			promedioTempGato=acumTempGato/cantGatos;
+
+			break;
+			case"otra cosa":
+			cantOtros++;
+			ultimaTipoOTra=nombre;
+			acumTempOtro=acumTempOtro+temperaturaCorporal;
+			promedioTempOtro=acumTempOtro/cantOtros;
+
+			break;
+		}
+		acumuladorKilos=acumuladorKilos+peso;
+		if(promedioTempPerro<promedioTempOtro&&promedioTempGato){
+			mayorPromedio="perros";
+		}
+			else if(promedioTempGato<promedioTempOtro){
+				mayorPromedio="gatos";
+			}
+				else{
+					mayorPromedio= "tipo otra cosa";
+				}
+		switch(estadoClinico){
+			case"internado":
+			cantInternados++;
+			break;
+			case"adopción":
+			cantAdopcion++;
+			break;
+			case"enfermo":
+			cantEnfermos++;
+			break
+		}
+		if(cantEnfermos>cantAdopcion&&cantInternados){
+			menorEstado="enfermos";
+		}
+			else if(cantAdopcion>cantInternados){
+				menorEstado="en adopción";
+			}
+				else{
+					menorEstado= "internados";
+				}
+		if(pelaje=="sin pelo"){
+			sinPelo++;
+			if(temperaturaCorporal<sinPeloFrio||bandera==0){
+				bandera=1;
+				sinPeloFrio=temperaturaCorporal;
+				animalFrioSinPelo=mascota;
+
+			}
+			if(mascota=="gato"){
+				cantGatoSinPelo++;
+				if(peso<sinPeloLiviano||bandera2==0){
+					bandera2=1;
+					sinPeloLiviano=peso;
+					razaSinPeloLiviano=raza;
+					nombreSinPeloLiviano=nombre;
+				}
+			}
+		}
+		promedioKilos=acumuladorKilos/(cantGatos+cantPerros+cantOtros)*100;
+		promedioCatDog=(cantPerros+cantGatos)/(cantPerros+cantGatos+cantOtros)*100;
+		respuesta=prompt("Desea ingresar los datos de otra persona: si/no");
+		promedioEnfermos=cantEnfermos/(cantAdopcion+cantEnfermos+cantInternados)*100;
+	}while(respuesta == "si"||respuesta=="Si"||respuesta=="Sí"||respuesta=="sí");
+	if(cantPerros!=0){
+		document.write("El perro mas pesado se llama "+nombrePerroPesado+".<br>");
+	}
+	if(cantEnfermos!=0){
+		document.write("El promedio de mascotas enfermas sobre el total es de "+promedioEnfermos+"%.<br>")
+	}
+	if (cantOtros!=0) {
+		document.write("La ultima mascota tipo otra en ingresar se llama "+ultimaTipoOTra+".<br>");
+	}
+	if(sinPelo!=0){
+		document.write("El animal sin pelo con menor temperatura corporal es de tipo "+animalFrioSinPelo+".<br>");
+	}
+	document.write("El mayor promedio de temperatura corporal de mascotas lo tienen los "+mayorPromedio+".<br>");
+	if (cantPerros!=0||cantGatos!=0) {
+		document.write("El promedio de cantidad de gatos y perros sobre otro tipo de mascotas es "+promedioCatDog+"%.<br>");
+	}
+	document.write("El estado clínico con menor cantidad de mascotas es"+menorEstado+".<br>");
+	document.write("El promedio de kilos de peso de todas las mascotas es "+promedioKilos+".<br>");
+	if(cantGatoSinPelo!=0){
+		document.write("El nombre del gato sin pelo mas liviano es "+nombreSinPeloLiviano+" y es de raza "+razaSinPeloLiviano+".<br>")
+	}
 }
--- 9-Parciales/2022_SimulacroIngres/seis.js
+++ 9-Parciales/2022_SimulacroIngres/seis.js
@@ -1,5 +1,129 @@
-
+/*Simulacro 6, Carnelos Duarte Joaquín Alejo*/
+/*Realizar el algoritmo que permita ingresar los datos de los alumnos de una división de la UTN FRA,
+los datos solicitados son:
+Nombre(validar no sea numero)
+Situación laboral ("buscando", "trabajando" o "solo estudiante")
+Cantidad de materias(más de cero y menos de 8)
+Sexo (femenino, masculino, no binario)
+Nota promedio (entre 0 y 10)
+Edad (validar entre 18 y 85 inclusive)
+Se debe informar de existir, o informar que no existe, en el caso que corresponda.
+a) El nombre del mejor promedio que no de los que trabajan o están buscando}
+b) El nombre del más viejo de los alumnos que solo sea estudiantes
+c) El promedio de nota por situación laboral
+d) La edad y nombre del que cursa menos cantidad de materias y que esté buscando trabajo*/
 function mostrar()
 {
-	alert("uno");
-}
+	let nombre;
+	let situacionLaboral;
+	let cantMaterias;
+	let sexo;
+	let nota;
+	let edad;
+	let mejorNoTrabaja=0;
+	let mejorNotrabajaNombre;
+	let genteBuscando=0;
+	let genteTrabajando=0;
+	let genteSoloEstudia=0;
+	let viejoEdadEstudia=0;
+	let nombreViejoEstudia;
+	let acumuladorNotaTrabaja=0;
+	let acumuladorNotaBuscando=0;
+	let acumuladorNotaSoloEstudia=0;
+	let promedioTrabaja;
+	let promedioBuscando;
+	let promedioSoloEstudia;
+	let MenosMaterias=0;
+	let edadMenos;
+	let nombreMenos;
+	let bandera=0;
+
+
+	do{
+		nombre=prompt("Ingrese nombre del alumno.");
+    	while(!(isNaN(nombre))){
+    		nombre=prompt("Por favor, Ingrese nombre real del alumno.");
+    	}
+    	situacionLaboral=prompt("Ingrese situacion laboral del alumno: buscando/trabajando/solo estudiante")
+    	while(!(situacionLaboral=="buscando"||situacionLaboral=="trabajando"||situacionLaboral=="solo estudiante")){
+    		situacionLaboral=prompt("Por favor, Ingrese situacion laboral del alumno: buscando/trabajando/solo estudiante")
+    	}
+    	cantMaterias=parseInt(prompt("Ingrese cantidad de materias(1-7)"));
+    	while(isNaN(cantMaterias)||cantMaterias<1||cantMaterias>8){
+    		cantMaterias=parseInt(prompt("Por favor, Ingrese cantidad de materias(1-7)"));
+    	}
+    	sexo=prompt("Ingrese sexo: femenino/masculino/no binario");
+    	while(!(sexo=="femenino"||sexo=="masculino"||sexo=="no binario")){
+      		sexo=prompt("Por favor,Ingrese sexo: femenino/masculino/no binario");
+    	}
+    	edad=parseInt(prompt("Ingrese años de edad(entre 18 y 85 inclusive)."));
+    	while(edad>85||edad<18||isNaN(edad)){
+      		edad=(prompt("Por favor,Ingrese años de edad del cliente(entre 18 y 85 inclusive)."));
+   		}
+   		nota=parseInt(prompt("Ingrese Nota promedio (entre 0 y 10)"));
+   		while(isNaN(nota)||(nota<0||nota>10)){
+      		nota=parseInt(prompt("Por favor, Ingrese Nota promedio (entre 0 y 10)"))
+  		}
+  		respuesta=prompt("Desea ingresar los datos de otro alumno: si/no");
+
+  		if(situacionLaboral=="buscando"||situacionLaboral=="solo estudiante"){
+  			if(nota>mejorNoTrabaja){
+  				mejorNoTrabaja=nota;
+  				mejorNotrabajaNombre=nombre;
+  			}
+  		}
+  		switch(situacionLaboral){
+  			case"buscando":
+  			genteBuscando++;
+  			acumuladorNotaBuscando=acumuladorNotaBuscando+nota;
+  			promedioBuscando=acumuladorNotaBuscando/genteBuscando;
+  			if(cantMaterias<MenosMaterias||bandera==0){
+  				edadMenos=edad;
+  				nombreMenos=nombre;
+  				MenosMaterias=cantMaterias;
+  				bandera=1;
+  			}
+  			break;
+  			case"solo estudiante":
+  			genteSoloEstudia++;
+  			acumuladorNotaSoloEstudia=acumuladorNotaSoloEstudia+nota;
+  			promedioSoloEstudia=acumuladorNotaSoloEstudia/genteSoloEstudia;
+  			if(nota>viejoEdadEstudia){
+  				viejoEdadEstudia=nota;
+  				nombreViejoEstudia=nombre
+  			}
+  			break;
+  			default:
+  			genteTrabajando++;
+  			acumuladorNotaTrabaja=acumuladorNotaTrabaja+nota;
+  			promedioTrabaja=acumuladorNotaTrabaja+nota;
+  		}
+  	}while(respuesta == "si"||respuesta=="Si"||respuesta=="Sí"||respuesta=="sí");
+
+  	if(genteBuscando!=0||genteSoloEstudia!=0){
+  		document.write("El nombre del mejor promedio de los que no trabajan es "+mejorNotrabajaNombre+".<br>")
+  	}
+  		else{
+  			document.write("No hay alumnos que no trabajen para sacar dicho promedio.<br>");
+  		}
+  	if(genteSoloEstudia!=0){
+  		document.write("El nobmre del alumno mas viejo que solo estudia es "+nombreViejoEstudia+".<br>");
+  		document.write("El promedio de los alumnos que solo estudian es "+promedioSoloEstudia+".<br>");
+  	}
+  		else{
+  			document.write("No hay alumnos que sean solo estudiantes para sacar un promedio ni identificar al mas viejo.<br>");
+  		}
+  	if(genteBuscando!=0){
+  		document.write("El promedio de los alumnos que buscan trabajo es "+promedioBuscando+".<br>");
+  		document.write("El nombre del alumno que cursa menos materias y busca trabajo es "+nombreMenos+"y tiene "+edadMenos+" años.<br>");
+  	}
+  		else{
+  			document.write("No hay alumnos que busquen trabajo para sacar un promedio de notas ni identificar al que menos materia cursa.<br>");
+  		}
+  	if(genteTrabajando!=0){
+  		document.write("El promedio de los alumnos trabajan es "+promedioTrabaja+".<br>");
+  	}
+  		else{
+  			document.write("No hay alumnos que trabajando para sacar un promedio.<br>");
+  		}
+  }
